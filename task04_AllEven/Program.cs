d7210c3ae960a8d9a393e2e517ec8e66a7235018
Marina <shalneva_mg@mail.ru>
2023-03-25 16:38:57 +0400

    Added Task 04 all even numbers from 1 to N

---
+++ task04_AllEven/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//  Например, 5 -> 2, 4
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+string even = "";
+
+if (number != 1 && number != 0)
+{
+    for (int i = 1; i < number + 1; i ++){
+        if (i % 2 == 0){
+            even = even + i.ToString() + " ";
+        }
+    }
+    Console.WriteLine(even);
+}
+else{
+    Console.WriteLine("Четных чисел нет");
+}
