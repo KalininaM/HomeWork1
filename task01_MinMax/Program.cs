b5a48b8fd0f30038b3167acb7352f356235aea8d
Marina <shalneva_mg@mail.ru>
2023-03-25 15:59:29 +0400

    Added Task 01 of issuing a Min and Max

---
+++ task01_MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7 min = 5
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int min = a;
+int max = b;
+
+if (a > b){
+    min = b;
+    max = a;
+}
+
+Console.WriteLine("max = " + max.ToString());
+Console.WriteLine("min = " + min.ToString());
